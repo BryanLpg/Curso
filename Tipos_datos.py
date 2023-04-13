186966e26c9d3f1273daffe563ae3fce9f2ef5c9
BryanLpg <130245367+BryanLpg@users.noreply.github.com>
2023-04-12 22:33:49 -0400

    Create Tipos_datos.py

---
+++ Tipos_datos.py
@@ -0,0 +1,7 @@
+entero=1 #Numeros enteros
+deCimal=1.1 #Float
+booleano=False #True
+cadena='Hola' #Cadena de texto 
+lista=['Cadena', 12, 1.1, False] #Lista (Pueden tener todo los tipos de datos dentro)
+tupla=(1, '3', 5) #Tupla (Pueden tener todo los tipos de datos dentro pero a diferencia de la lista una vez ejecutado el programa no se pueden cambiar)
+diccionario_navegadores={	'Chrome'	: 	79	 , 	'Firefox'	: 	'v71'	} #Diccionario (Pueden tener todo los tipos de datos dentro con su Clave: valor)
